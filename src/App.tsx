16c791f505bd1d0cc6b03b433642ffa7eb091c0f
alexandre <alexandrelima2021@outlook.com>
2023-05-30 15:07:05 -0300

    Adicionando a biblioteca switch

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { ThemeProvider } from "styled-components";
+import light from "./styles/themes/light";
+import dark from "./styles/themes/dark";
+
+import GlobalStyle from "./styles/global";
+import Header from "./Components/Header";
+
+const App = () => {
+  const [theme, setTheme] = useState(light);
+
+  const toggleTheme = () => {
+    setTheme(theme.title === "light" ? dark : light);
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Header toggleTheme={toggleTheme} />
+    </ThemeProvider>
+  );
+};
+
+export default App;
---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,11 @@
+export default {
+  title: "dark",
+
+  colors: {
+    primary: "#D36582",
+    secundary: "#FFEECF",
+
+    background: "#222",
+    text: "#FFF",
+  },
+};
